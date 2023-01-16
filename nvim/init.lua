671d3e16842740aa00c6b8e5a43188d9cd374396
korniii <moritz.kornmeier@gmail.com>
2023-01-16 21:01:49 +0100

    finished lazy migration

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,9 +2,3 @@ require("config")
 
 -- reminder: syntax folder for terraform syntax highlighting since treesitter seems to not be working on .tf files check again later
 -- fix from https://www.reddit.com/r/vim/comments/wl29ng/syntax_highlighting_for_terraform_hcl/
-
--- comments
---require('Comment').setup()
-
--- git
---require('gitsigns').setup() -- add keymaps and stuff
