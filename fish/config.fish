02e8ce134dd827d27ba012d49e38c29aadbeed33
okr7fe <moritz.kornmeier@de.bosch.com>
2023-01-16 15:32:53 +0100

    added proxy

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,10 @@
 . ~/.config/fish/config.shortcuts
 
+if test -f ~/.config/fish/proxy.fish
+    . ~/.config/fish/proxy.fish
+end
+
+
 # if status is-interactive
     # Commands to run in interactive sessions can go here
 # end
