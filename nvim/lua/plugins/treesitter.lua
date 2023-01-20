a9ca0a6834a3fa6f52523d3ce2e456bcd434b1e5
okr7fe <moritz.kornmeier@de.bosch.com>
2023-01-20 15:54:09 +0100

    fixed treesitter and modified tmux.conf

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,38 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	build = function()
-		require('nvim-treesitter.install').update({ with_sync = true })
-	end,
+	config = function()
+		require 'nvim-treesitter.configs'.setup {
+			ensure_installed = {
+				"bash",
+				"css",
+				"fish",
+				"gitignore",
+				"go",
+				"graphql",
+				"help",
+				"html",
+				"http",
+				"java",
+				"javascript",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"python",
+				"regex",
+				"rust",
+				"scss",
+				"sql",
+				"toml",
+				"tsx",
+				"typescript",
+				"vim",
+				"vue",
+				"yaml",
+				"json",
+			},
+			highlight = {
+				enable = true,
+			},
+		}
+	end
 }
