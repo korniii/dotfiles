86f44cbf72c7cfc580a040b31d482b764695642d
okr7fe <moritz.kornmeier@de.bosch.com>
2023-03-07 15:48:42 +0100

    added git functionalities

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -3,17 +3,14 @@ return {
 	dependencies = {
 		"nvim-telescope/telescope.nvim"
 	},
-  init = function()
+	init = function()
+		local builtin = require("telescope.builtin")
 
-    local builtin = require("telescope.builtin")
-
-    vim.keymap.set("n", "<leader>sf", builtin.find_files, {desc = "[S]earch [F]iles"})
-    vim.keymap.set("n", "<leader>sr", builtin.live_grep, {desc = "[S]earch in Files with [R]ipgrep"})
-    vim.keymap.set("n", "<leader>sd", builtin.diagnostics, {desc = "[S]earch [D]iagnostics"})
-    vim.keymap.set("n", "<leader>sb", builtin.current_buffer_fuzzy_find, {desc = "[S]earch in [B]uffer"})
-    vim.keymap.set("n", "<leader>sg", builtin.git_files, {desc = "[S]earch in [G]it Files"})
-    vim.keymap.set("n", "<leader>ss", builtin.grep_string, {desc = "[S]earch current [S]tring"})
-
-  end
+		vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+		vim.keymap.set("n", "<leader>sr", builtin.live_grep, { desc = "[S]earch in Files with [R]ipgrep" })
+		vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+		vim.keymap.set("n", "<leader>sb", builtin.current_buffer_fuzzy_find, { desc = "[S]earch in [B]uffer" })
+		vim.keymap.set("n", "<leader>sg", builtin.git_files, { desc = "[S]earch in [G]it Files" })
+		vim.keymap.set("n", "<leader>ss", builtin.grep_string, { desc = "[S]earch current [S]tring" })
+	end
 }
-
