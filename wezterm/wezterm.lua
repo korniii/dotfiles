5a9644cdbbdbcfcab3f32521a7a2b2fddfd48388
Moritz Kornmeier <moritz.kornmeier@bosch.com>
2023-05-02 15:29:12 +0200

    added wezterm cfg

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,12 @@
+local wezterm = require("wezterm")
+
+local config = {
+	color_scheme = 'nord',
+	window_background_opacity = 0.80,
+	hide_tab_bar_if_only_one_tab = true,
+	window_decorations = "RESIZE",
+	font_size = 15.0,
+	native_macos_fullscreen_mode = true,
+}
+
+return config
