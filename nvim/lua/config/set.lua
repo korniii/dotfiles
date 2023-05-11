3efaeb599c3fdf35bdd9577d74654ff0c72cc6ad
Moritz Kornmeier <moritz.kornmeier@bosch.com>
2023-05-11 14:13:43 +0200

    fixed ufo cfg

--- nvim/lua/config/set.lua
+++ nvim/lua/config/set.lua
@@ -12,3 +12,16 @@ vim.opt.spell = true
 vim.opt.shiftwidth = 2
 vim.bo.expandtab = true
 vim.opt.tabstop = 2
+
+vim.opt.fillchars = {
+  foldopen = "",
+  foldclose = "",
+  fold = " ",
+  foldsep = " ",
+  diff = "╱",
+  eob = " ",
+}
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+vim.o.foldcolumn = "0"
--- nvim/lua/plugins/ufo.lua
+++ nvim/lua/plugins/ufo.lua
@@ -1,37 +1,55 @@
 return {
-  'kevinhwang91/nvim-ufo',
-  dependencies = {
-    'kevinhwang91/promise-async'
-  },
-  event = "BufRead",
-  keys = {
-    { "zR", function() require('ufo').openAllFolds() end, desc = "Open all folds" },
-    { "zM", function() require('ufo').closeAllFolds() end, desc = "Close all folds" },
-    { "zp", function() require('ufo').peekFoldedLinesUnderCursor() end, desc = "Peed folded lines under cursor" },
-  },
-  opts = {
-    open_fold_hl_timeout = 0,
-    fold_virt_text_handler = function(text, lnum, endLnum, width)
-      local suffix = "  "
-      local lines  = ('[%d lines] '):format(endLnum - lnum)
+	'kevinhwang91/nvim-ufo',
+	dependencies = {
+		'kevinhwang91/promise-async'
+	},
+	opts = {
+		open_fold_hl_timeout = 0,
+		fold_virt_text_handler = function(text, lnum, endLnum, width)
+			local suffix    = "  "
+			local lines     = ('[%d lines] '):format(endLnum - lnum)
 
-      local cur_width = 0
-      for _, section in ipairs(text) do
-        cur_width = cur_width + vim.fn.strdisplaywidth(section[1])
-      end
+			local cur_width = 0
+			for _, section in ipairs(text) do
+				cur_width = cur_width + vim.fn.strdisplaywidth(section[1])
+			end
 
-      suffix = suffix .. (' '):rep(width - cur_width - vim.fn.strdisplaywidth(lines) - 3)
+			suffix = suffix .. (' '):rep(width - cur_width - vim.fn.strdisplaywidth(lines) - 3)
 
-      table.insert(text, { suffix, 'Comment' })
-      table.insert(text, { lines, 'Todo' })
-      return text
-    end,
-    preview = {
-      win_config = {
-        border = {"┏", "━", "┓", "┃", "┛", "━", "┗", "┃"},
-        winblend     = 0,
-        winhighlight = "Normal:LazyNormal",
-      }
-    }
-  },
+			return text
+		end,
+		close_fold_kinds = { 'imports', 'comment' },
+		preview = {
+			win_config = {
+				border       = { "┏", "━", "┓", "┃", "┛", "━", "┗", "┃" },
+				winblend     = 0,
+				winhighlight = "Normal:LazyNormal",
+			}
+		}
+	},
+	init = function()
+		vim.keymap.set("n", "zR", function()
+			require("ufo").openAllFolds()
+		end)
+		vim.keymap.set("n", "zM", function()
+			require("ufo").closeAllFolds()
+		end)
+		vim.keymap.set("n", "zp", function()
+			require("ufo").peekFoldedLinesUnderCursor()
+		end)
+
+		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		capabilities.textDocument.foldingRange = {
+			dynamicRegistration = false,
+			lineFoldingOnly = true
+		}
+		local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
+		for _, ls in ipairs(language_servers) do
+			require('lspconfig')[ls].setup({
+				capabilities = capabilities
+				-- you can add other fields for setting up lsp server in this table
+			})
+		end
+		require('ufo').setup()
+	end
 }
