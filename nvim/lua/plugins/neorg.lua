72364fe4951fbbcf8ed5bbb5d88626b38f0f5ad1
korniii <moritz.kornmeier@gmail.com>
2023-06-27 22:52:18 +0200

    added neorg

---
+++ nvim/lua/plugins/neorg.lua
@@ -0,0 +1,35 @@
+return {
+	"nvim-neorg/neorg",
+	build = ":Neorg sync-parsers",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-neorg/neorg-telescope",
+	},
+	config = function()
+		require("neorg").setup {
+			load = {
+				["core.defaults"] = {}, -- Loads default behaviour
+				["core.keybinds"] = {
+					config = {
+						neorg_leader = " "
+					}
+				},
+				["core.concealer"] = {}, -- Adds pretty icons to your documents
+				["core.dirman"] = {  -- Manages Neorg workspaces
+					config = {
+						workspaces = {
+							notes = "~/notes",
+						},
+						default_workspace = "notes",
+					},
+				},
+				["core.journal"] = {
+					config = {
+						workspace = "notes",
+						folder_name = "journal",
+					}
+				}
+			},
+		}
+	end,
+}
