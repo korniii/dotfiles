cab015b1fd0a30df2be7c5b85cbfd08ddd867287
okr7fe <moritz.kornmeier@bosch.com>
2023-11-02 07:10:57 +0100

    remove conceal on markdown files

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -2,10 +2,19 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
+-- save buffer if focus is lost
 vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost" }, {
   callback = function()
     if vim.bo.modified and not vim.bo.readonly and vim.fn.expand("%") ~= "" and vim.bo.buftype == "" then
       vim.api.nvim_command("silent update")
     end
+  end,
+})
+
+-- remove conceal on markdown files
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "markdown" },
+  callback = function()
+    vim.wo.conceallevel = 0
   end,
 })
