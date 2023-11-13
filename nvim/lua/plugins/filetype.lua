7a2a7acc2cc9c02600b732f9cff422ff07dda7ea
okr7fe <moritz.kornmeier@bosch.com>
2023-11-13 15:08:12 +0100

    changed filetype.lua and neorg.lua

--- nvim/lua/plugins/filetype.lua
+++ nvim/lua/plugins/filetype.lua
@@ -1,22 +1,26 @@
 return {
-	"nathom/filetype.nvim",
-	opts = {
-		overrides = {
-			extensions = {
-				sh = "bash",
-				tf = "terraform",
-				tfvars = "terraform",
-				tfstate = "json",
-			},
-			complex = {
-				[".*/templates/.*%.yaml"] = "helm",
-				[".*/templates/.*%.tpl"] = "helm",
-				["helmfile.yaml"] = "helm",
-			},
-			shebang = {
-				dash = "sh",
-			},
-		},
-		config = true
-	}
+  "nathom/filetype.nvim",
+  opts = {
+    overrides = {
+      extensions = {
+        sh = "bash",
+        tf = "terraform",
+        tfvars = "terraform",
+        tfbackend = "terraform",
+        tfstate = "json",
+      },
+      literal = {
+        [".envrc"] = "bash",
+      },
+      complex = {
+        [".*/templates/.*%.yaml"] = "helm",
+        [".*/templates/.*%.tpl"] = "helm",
+        ["helmfile.yaml"] = "helm",
+      },
+      shebang = {
+        dash = "sh",
+      },
+    },
+    config = true,
+  },
 }
