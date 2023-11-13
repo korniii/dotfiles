8348c4b81ff3dca6a0753627df490aa6045c076d
okr7fe <moritz.kornmeier@bosch.com>
2023-11-13 15:06:54 +0100

    changed wezterm fontsize

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,11 +1,11 @@
 local wezterm = require("wezterm")
 
 local config = {
-	color_scheme = 'Atelier Dune (base16)',
+	color_scheme = "Atelier Dune (base16)",
 	window_background_opacity = 0.80,
 	hide_tab_bar_if_only_one_tab = true,
 	window_decorations = "RESIZE",
-	font_size = 15.0,
+	font_size = 18.0,
 	native_macos_fullscreen_mode = true,
 }
 
