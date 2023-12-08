1305dff371dae9776fc4cc1c23668f334886d390
okr7fe <moritz.kornmeier@bosch.com>
2023-12-08 19:33:12 +0100

    fixed ufo setup

--- nvim/lua/plugins/ufo.lua
+++ nvim/lua/plugins/ufo.lua
@@ -1,70 +1,74 @@
 return {
-	'kevinhwang91/nvim-ufo',
-	dependencies = {
-		'kevinhwang91/promise-async'
-	},
-	event = "BufReadPost",
-	opts = {
-		open_fold_hl_timeout = 0,
-		fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
-			local newVirtText = {}
-			local suffix = ('  %d '):format(endLnum - lnum)
-			local sufWidth = vim.fn.strdisplaywidth(suffix)
-			local targetWidth = width - sufWidth
-			local curWidth = 0
-			for _, chunk in ipairs(virtText) do
-				local chunkText = chunk[1]
-				local chunkWidth = vim.fn.strdisplaywidth(chunkText)
-				if targetWidth > curWidth + chunkWidth then
-					table.insert(newVirtText, chunk)
-				else
-					chunkText = truncate(chunkText, targetWidth - curWidth)
-					local hlGroup = chunk[2]
-					table.insert(newVirtText, { chunkText, hlGroup })
-					chunkWidth = vim.fn.strdisplaywidth(chunkText)
-					-- str width returned from truncate() may less than 2nd argument, need padding
-					if curWidth + chunkWidth < targetWidth then
-						suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
-					end
-					break
-				end
-				curWidth = curWidth + chunkWidth
-			end
-			table.insert(newVirtText, { suffix, 'MoreMsg' })
-			return newVirtText
-		end,
-		close_fold_kinds = { 'imports', 'comment' },
-		preview = {
-			win_config = {
-				border       = { "┏", "━", "┓", "┃", "┛", "━", "┗", "┃" },
-				winblend     = 0,
-				winhighlight = "Normal:LazyNormal",
-			}
-		}
-	},
-	init = function()
-		vim.keymap.set("n", "zR", function()
-			require("ufo").openAllFolds()
-		end)
-		vim.keymap.set("n", "zM", function()
-			require("ufo").closeAllFolds()
-		end)
-		vim.keymap.set("n", "zp", function()
-			require("ufo").peekFoldedLinesUnderCursor()
-		end)
+  -- add folding range to capabilities
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      capabilities = {
+        textDocument = {
+          foldingRange = {
+            dynamicRegistration = false,
+            lineFoldingOnly = true,
+          },
+        },
+      },
+    },
+  },
+  {
+    "kevinhwang91/nvim-ufo",
+    dependencies = {
+      "kevinhwang91/promise-async",
+    },
+    event = "BufReadPost",
+    opts = {
+      open_fold_hl_timeout = 0,
+      fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
+        local newVirtText = {}
+        local suffix = ("  %d "):format(endLnum - lnum)
+        local sufWidth = vim.fn.strdisplaywidth(suffix)
+        local targetWidth = width - sufWidth
+        local curWidth = 0
+        for _, chunk in ipairs(virtText) do
+          local chunkText = chunk[1]
+          local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+          if targetWidth > curWidth + chunkWidth then
+            table.insert(newVirtText, chunk)
+          else
+            chunkText = truncate(chunkText, targetWidth - curWidth)
+            local hlGroup = chunk[2]
+            table.insert(newVirtText, { chunkText, hlGroup })
+            chunkWidth = vim.fn.strdisplaywidth(chunkText)
+            -- str width returned from truncate() may less than 2nd argument, need padding
+            if curWidth + chunkWidth < targetWidth then
+              suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+            end
+            break
+          end
+          curWidth = curWidth + chunkWidth
+        end
+        table.insert(newVirtText, { suffix, "MoreMsg" })
+        return newVirtText
+      end,
+      close_fold_kinds = { "imports", "comment" },
+      preview = {
+        win_config = {
+          border = { "┏", "━", "┓", "┃", "┛", "━", "┗", "┃" },
+          winblend = 0,
+          winhighlight = "Normal:LazyNormal",
+        },
+      },
+    },
+    init = function()
+      vim.keymap.set("n", "zR", function()
+        require("ufo").openAllFolds()
+      end)
+      vim.keymap.set("n", "zM", function()
+        require("ufo").closeAllFolds()
+      end)
+      vim.keymap.set("n", "zp", function()
+        require("ufo").peekFoldedLinesUnderCursor()
+      end)
 
-		local capabilities = vim.lsp.protocol.make_client_capabilities()
-		capabilities.textDocument.foldingRange = {
-			dynamicRegistration = false,
-			lineFoldingOnly = true
-		}
-		local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
-		for _, ls in ipairs(language_servers) do
-			require('lspconfig')[ls].setup({
-				capabilities = capabilities
-				-- you can add other fields for setting up lsp server in this table
-			})
-		end
-		require('ufo').setup()
-	end
+      require("ufo").setup()
+    end,
+  },
 }
