63a76e943fe15992d387df2398bf1eb5e92abc6b
okr7fe <moritz.kornmeier@bosch.com>
2024-03-05 09:29:41 +0100

    updated wezterm padding

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,6 +8,12 @@ local config = {
 	font = wezterm.font("JetBrains Mono"),
 	font_size = 18.0,
 	native_macos_fullscreen_mode = true,
+	window_padding = {
+		left = 15,
+		right = 15,
+		top = 20,
+		bottom = 0,
+	},
 }
 
 return config
