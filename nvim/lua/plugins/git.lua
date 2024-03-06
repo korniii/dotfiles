267d1b7f1e00fdd9fe890c6869f8246a9b7f8040
okr7fe <moritz.kornmeier@bosch.com>
2024-03-06 08:59:00 +0100

    changed gitsigns config

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,5 +1,18 @@
 return {
   {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      signs = {
+        add = { text = "+" },
+        change = { text = "~" },
+        delete = { text = "-" },
+        topdelete = { text = "" },
+        changedelete = { text = "_" },
+        untracked = { text = "?" },
+      },
+    },
+  },
+  {
     "sindrets/diffview.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
