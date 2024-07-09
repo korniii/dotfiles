e0aa36fd5575f4a86e8a04f68906f6175e348719
korniii <moritz.kornmeier@gmail.com>
2024-07-09 08:26:38 +0200

    added fzf to sources

--- fish/config.fish
+++ fish/config.fish
@@ -4,35 +4,36 @@ end
 
 fish_add_path ~/.config/scripts
 
-bind \cf "tmux-sessionizer"
+bind \cf tmux-sessionizer
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    fzf --fish | source
     atuin init fish | source
     starship init fish | source
 end
 
 abbr -a ll "ls -la"
-abbr v "nvim"
-abbr g "lazygit"
-abbr k "k9s"
+abbr v nvim
+abbr g lazygit
+abbr k k9s
 
-abbr tf "terraform"
+abbr tf terraform
 abbr tfi "terraform init"
 abbr tfp "terraform plan"
 abbr tfa "terraform apply"
 
 if type -q eza
-	abbr -a l "eza"
-	abbr -a ls "eza"
-	abbr -a ll "eza -l"
-	abbr -a lll "eza -la"
-else 
-	abbr -a l "ls"
-	abbr -a ll "ls -l"
-	abbr -a lll "ls -la"
+    abbr -a l eza
+    abbr -a ls eza
+    abbr -a ll "eza -l"
+    abbr -a lll "eza -la"
+else
+    abbr -a l ls
+    abbr -a ll "ls -l"
+    abbr -a lll "ls -la"
 end
 
 if type -q bat
-	abbr -a cat "bat"
+    abbr -a cat bat
 end
