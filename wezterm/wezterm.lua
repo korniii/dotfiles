e1a0ad01614335fee5ec16f1278636ae5c3c9c75
okr7fe <moritz.kornmeier@bosch.com>
2024-10-21 14:16:58 +0200

    added aerospace for window tiling

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 
 local config = {
-	default_prog = { '/opt/homebrew/bin/fish' },
+	default_prog = { "/opt/homebrew/bin/fish" },
 	color_scheme = "Atelier Dune (base16)",
 	window_background_opacity = 0.80,
 	hide_tab_bar_if_only_one_tab = true,
@@ -10,9 +10,9 @@ local config = {
 	font_size = 18.0,
 	native_macos_fullscreen_mode = true,
 	window_padding = {
-		left = 15,
-		right = 15,
-		top = 20,
+		left = 4,
+		right = 0,
+		top = 6,
 		bottom = 0,
 	},
 }
