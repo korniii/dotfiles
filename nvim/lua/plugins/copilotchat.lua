45e3209df0cd237472a4e09a138eb58d2c399e47
okr7fe <moritz.kornmeier@bosch.com>
2026-02-02 16:35:39 +0100

    replace copilot default model

--- nvim/lua/plugins/copilotchat.lua
+++ nvim/lua/plugins/copilotchat.lua
@@ -3,7 +3,7 @@ return {
     "CopilotC-Nvim/CopilotChat.nvim",
     build = "make tiktoken",
     opts = {
-      model = "claude-sonnet-4.5",
+      model = "claude-opus-4.5",
       prompts = {
         Grammar = {
           prompt = "Fix grammar and typos of the text.",
